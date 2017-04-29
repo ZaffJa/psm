ebe80459608a6c46592ee973193f8750718e0fd2
Zafri Zulkipli <zafrizulkipli@gmail.com>
2017-04-30 02:50:12 +0800

    Updated dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,15 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\Transaction;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function view()
     {
-        $users = User::all();
-        $ride_cashflow = Transaction::where('');
-        return view('dashboard');
+        $users = User::whereDate('created_at', Carbon::now()->format('Y-m-d'))->count();
+
+        $get_rides = Transaction::select('price')
+            ->where('request_type', 1)
+            ->where('status', 2)
+            ->whereDate('updated_at', Carbon::now()->format('Y-m-d'))
+            ->sum('price');
+
+        $get_cars = Transaction::select('price')
+            ->where('request_type', 2)
+            ->where('status', 2)
+            ->whereDate('updated_at', Carbon::now()->format('Y-m-d'))
+            ->sum('price');
+
+        return view('dashboard',compact('users','get_rides','get_cars'));
     }
 
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
                 <i class="icon fa fa-shopping-basket fa-4x"></i>
                 <div class="content">
                     <div class="title">Ride Cash Flow Today</div>
-                    <div class="value"><span class="sign">RM </span>4</div>
+                    <div class="value"><span class="sign">RM </span>{{ $get_rides }}</div>
                 </div>
             </div>
         </a>
@@ -19,7 +19,7 @@
                 <i class="icon fa fa-thumbs-o-up fa-4x"></i>
                 <div class="content">
                     <div class="title">Rents Cash Flow Today</div>
-                    <div class="value"><span class="sign">RM </span>10</div>
+                    <div class="value"><span class="sign">RM </span>{{ $get_cars }}</div>
                 </div>
             </div>
         </a>
@@ -30,7 +30,7 @@
                 <i class="icon fa fa-user-plus fa-4x"></i>
                 <div class="content">
                     <div class="title">New Registration</div>
-                    <div class="value"><span class="sign"></span>0</div>
+                    <div class="value"><span class="sign"></span>{{ $users }}</div>
                 </div>
             </div>
         </a>
